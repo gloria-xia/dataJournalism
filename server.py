0c1cdceb4e1a4d234471f2022c83fa297da7f837
gloria-xia <gloria.chenxi.xia@icloud.com>
2023-06-07 13:35:38 -0400

    updated data

--- server.py
+++ server.py
@@ -1,17 +1,26 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import json
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')
 def about():
-    #load a current view of the data
-    #render the template with the apporpriate data
+
+
     return render_template('about.html')
 
 @app.route('/macro')
 def macro():
+
+    f = open("data/csvjson.json", "r")
+    data = json.load(f)
+    f.close()
+
+    for point in data:
+        print(point["Year"])
+        print(point['Month'])
 
     return render_template('macro.html')
 
